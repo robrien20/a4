eb2c5ce855500cc0e6ab26d1161872bd9733aae8
Robert O'Brien <robrien20@gmail.com>
2019-03-10 14:59:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function ridgelinePlot(responses) {
     function handleMouseOver(d, i) { //d: data in format {month, value}, i: index
       d3.select(this).attr("class", "hover");
       d3.select(".text--info").style('visibility', 'visible');
-      d3.select(".text--info").html(info[i]['park'] + '<br>') //display name of park at bottom
+      d3.select(".text--info").html(info[i]['park']) //display name of park at bottom
           .append("text")
               .attr("class", "text--blurb")
               .html("Number of acres: " + info[i]['acres'].toLocaleString())
