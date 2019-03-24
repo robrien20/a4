9ad0c0c364d11784984cd66f13af22eeeb47805e
Robert O'Brien <robrien20@gmail.com>
2019-03-24 16:46:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function ridgelinePlot(responses) {
       d3.select(".text--info").html(info[i]['park'].toString()) //display name of park at bottom
           .append("text")
               .attr("class", "text--blurb")
-              .html("<br>" + "Number of acres: " + info[i]['acres'].toLocaleString() + <br> + "Peak visitation month: " + info[i]['peak visitation'])
+              .html("<br>" + "Number of acres: " + info[i]['acres'].toLocaleString() + "<br>" + "Peak visitation month: " + info[i]['peak visitation'])
           .append("text")
               .attr("class", "text--extra")
               .html( "<br>" + info[i]['fun fact']);
